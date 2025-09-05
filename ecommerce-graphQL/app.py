8b2bb3e7407db6e305aa7427a2801d7bcbb8e4e3
Dzkzzz <dzdzaakk@gmail.com>
2025-09-05 22:54:34 +0700

    add product

--- ecommerce-graphQL/app.py
+++ ecommerce-graphQL/app.py
@@ -5,21 +5,21 @@ from datetime import datetime
 
 # == Data Models ==
 products_db = [
- {
+    {
         "id": "1",
         "name": "Laptop Gaming ASUS ROG",
         "price": 15000000,
         "stock": 5,
         "category": "Electronics",
-        "description": "High-end gaming laptop with RTX 4060"
+        "description": "High-end gaming laptop with RTX 4060",
     },
     {
-        "id": "2", 
+        "id": "2",
         "name": "iPhone 15 Pro",
         "price": 20000000,
         "stock": 10,
         "category": "Electronics",
-        "description": "Latest iPhone with chip A17 Pro"
+        "description": "Latest iPhone with chip A17 Pro",
     },
     {
         "id": "3",
@@ -27,8 +27,8 @@ products_db = [
         "price": 2500000,
         "stock": 3,
         "category": "Fashion",
-        "description": "Limited edition sneakers"
-    }
+        "description": "Limited edition sneakers",
+    },
 ]
 
 orders_db = []
@@ -38,15 +38,18 @@ order_counter = 1
 # == GRAPHQL Types ==
 class Product(graphene.ObjectType):
     """Type for Product"""
+
     id = graphene.ID()
     name = graphene.String()
     price = graphene.Int()
     stock = graphene.Int()
     category = graphene.String()
     description = graphene.String()
-    
+
+
 class Order(graphene.ObjectType):
     """Type for Order"""
+
     id = graphene.ID()
     product_id = graphene.String()
     product_name = graphene.String()
@@ -56,15 +59,18 @@ class Order(graphene.ObjectType):
     status = graphene.String()
     created_at = graphene.String()
 
+
 # == QUERIES ==
-class Query(graphene.ObjectType ):
+class Query(graphene.ObjectType):
     """Query for Get Data"""
 
     all_products = graphene.List(Product)
 
     product = graphene.Field(Product, id=graphene.String(required=True))
 
-    products_by_category = graphene.List(Product, category=graphene.String(required=True))
+    products_by_category = graphene.List(
+        Product, category=graphene.String(required=True)
+    )
 
     all_orders = graphene.List(Order)
 
@@ -72,7 +78,7 @@ class Query(graphene.ObjectType ):
         Product,
         keyword=graphene.String(required=True),
         min_price=graphene.Int(),
-        max_price=graphene.Int()
+        max_price=graphene.Int(),
     )
 
     def resolve_all_products(self, info):
@@ -93,24 +99,28 @@ class Query(graphene.ObjectType ):
     def resolve_search_products(self, info, keyword, min_price=None, max_price=None):
         result = []
         for product in products_db:
-            if keyword.lower() in product["name"].lower() or \
-            keyword.lower() in product["description"].lower():
+            if (
+                keyword.lower() in product["name"].lower()
+                or keyword.lower() in product["description"].lower()
+            ):
 
-                if min_price and product["price"] < min_price: 
+                if min_price and product["price"] < min_price:
                     continue
-                if max_price and product["price"] >  max_price: 
+                if max_price and product["price"] > max_price:
                     continue
                 result.append(product)
         return result
 
+
 # == MUTATIONS ==
 class CreateOrder(graphene.Mutation):
     """Mutation for create order"""
+
     class Arguments:
         product_id = graphene.String(required=True)
-        quantity= graphene.String(required=True)
-        customer_name= graphene.String(required=True)
-    
+        quantity = graphene.String(required=True)
+        customer_name = graphene.String(required=True)
+
     order = graphene.Field(Order)
     success = graphene.Boolean()
     message = graphene.String()
@@ -123,17 +133,17 @@ class CreateOrder(graphene.Mutation):
             if p["id"] == product_id:
                 product = p
                 break
-        
+
         if not product:
             return CreateOrder(order=None, success=False, message="Product not found")
-        
-        if product["stock"] < quantity: 
+
+        if product["stock"] < quantity:
             return CreateOrder(
                 order=None,
                 success=False,
-                message=f"Insufficient stock. Available: {product['stock']}"
+                message=f"Insufficient stock. Available: {product['stock']}",
             )
-        
+
         new_order = {
             "id": str(order_counter),
             "product_id": product_id,
@@ -142,7 +152,7 @@ class CreateOrder(graphene.Mutation):
             "total_price": product["price"] * quantity,
             "customer_name": customer_name,
             "status": "PENDING",
-            "created_at": datetime.now().isoformat()
+            "created_at": datetime.now().isoformat(),
         }
 
         product["stock"] -= quantity
@@ -151,37 +161,56 @@ class CreateOrder(graphene.Mutation):
         order_counter += 1
 
         return CreateOrder(
-            order=new_order,
-            success=True,
-            message="Order created successfully"
+            order=new_order, success=True, message="Order created successfully"
         )
+
 
 class UpdateProductStock(graphene.Mutation):
     """Mutation for update stock product"""
+
     class Arguments:
         product_id = graphene.String(required=True)
         new_stock = graphene.Int(required=True)
-    
+
     product = graphene.Field(Product)
     success = graphene.Boolean()
-    message= graphene.String()
+    message = graphene.String()
 
     def mutate(self, info, product_id, new_stock):
         for product in products_db:
             if product["id"] == product_id:
                 product["stock"] == new_stock
                 return UpdateProductStock(
-                    product=product,
-                    success=True,
-                    message="Stock updated successfully"
+                    product=product, success=True, message="Stock updated successfully"
                 )
-        
+
         return UpdateProductStock(
-            product=None,
-            success=False,
-            message="Product not found"
+            product=None, success=False, message="Product not found"
         )
 
 
+class AddProduct(graphene.Mutation):
+    """Mutation for add new product"""
 
+    class Arguments:
+        name = graphene.String(required=True)
+        price = graphene.Int(required=True)
+        stock = graphene.Int(required=True)
+        category = graphene.String(required=True)
+        description = graphene.String()
 
+    product = graphene.Field(Product)
+    success = graphene.Boolean()
+
+    def mutate(self, info, name, price, stock, category, description=""):
+        new_product = {
+            "id": str(len(products_db) + 1),
+            "name": name,
+            "price": price,
+            "stock": stock,
+            "category": category,
+            "description": description,
+        }
+        products_db.append(new_product)
+
+        return AddProduct(product=new_product, success=True)
