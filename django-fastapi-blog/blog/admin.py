8c186f9133ac2c3baf7f28eb6f3d413ce635d169
Dzkzzz <dzdzaakk@gmail.com>
2025-09-10 20:48:25 +0700

    register models in admin

--- django-fastapi-blog/blog/admin.py
+++ django-fastapi-blog/blog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Post, Comment
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug", "created_at"]
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ["name"]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "author",
+        "category",
+        "status",
+        "created_at",
+        "published_at",
+    ]
+    list_filter = ["status", "created_at", "published_at", "category"]
+    search_fields = ["title", "content"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "created_at"
+    ordering = ["status", "-created_at"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["author", "post", "created_at", "is_active"]
+    list_filter = ["is_active", "created_at"]
+    search_fields = ["author__username", "content"]
