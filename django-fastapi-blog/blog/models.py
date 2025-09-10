6f6d82078a0e11ddf412c265391f40c2d69bc71f
Dzkzzz <dzdzaakk@gmail.com>
2025-09-10 20:41:35 +0700

    fix typ in models

--- django-fastapi-blog/blog/models.py
+++ django-fastapi-blog/blog/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-# Create your models here.
-
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -21,7 +19,8 @@ class Category(models.Model):
 
 class Post(models.Model):
     STATUS_CHOICES = [
-        ("draft", "Draft")("published", "Published"),
+        ("draft", "Draft"),
+        ("published", "Published"),
     ]
 
     title = models.CharField(max_length=200)
@@ -51,7 +50,7 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
 
-class Coment(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
